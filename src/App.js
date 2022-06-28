85d578cff488a32ed178890ce50dfc72e3dd44c4
YousraHiba <hibaouili34@gmail.com>
2022-06-28 13:47:55 +0200

    -Change App and Index.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 
+import "./App.css";
+import NewReleases from "./components/newReleases/NewReleases";
+import HeaderBar from "./components/shared/HeaderBar";
+import TrendingMovies from "./components/trending/TrendingMovies";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import LoginUser from "./views/authentication/LoginUser";
+import Home from "./views/home/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <HeaderBar />
+      <Routes>
+        <Route exact path="/user" element={<LoginUser />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
