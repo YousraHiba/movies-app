5cb48cc54a4900a6efd951d891330532547a508c
YousraHiba <hibaouili34@gmail.com>
2022-07-05 10:14:02 +0200

    -Modifie the login view.

--- src/views/authentication/LoginUser.js
+++ src/views/authentication/LoginUser.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./LoginUser.css";
 
 function LoginUser() {
   return (
-    <div>
-      <h1>HELLO FROM LOGIN USER</h1>
+    <div className="container form-container ">
+      <h2 className="title"> Registration</h2>
+      <div class=" m-4">
+        <label class="form-label">Email or username</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Name@email.com"
+        />
+      </div>
+      <div className=" m-4">
+        <label class="form-label">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="**********"
+        />
+      </div>
+      <button className="btn login-button">Sign In</button>
     </div>
   );
 }
